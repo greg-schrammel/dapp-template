4b283139c1c450fb512c71d589b6d2b7a623617a
gregs <ukfaeku@gmail.com>
2022-10-17 11:53:18 -0300

    fix hydration errors

--- packages/ui/components/Theme/ToggleTheme.tsx
+++ packages/ui/components/Theme/ToggleTheme.tsx
@@ -1,12 +1,14 @@
+import { useIsMounted } from 'hooks'
 import { MoonIcon, SunIcon } from 'icons'
 import { useTheme } from 'next-themes'
 import { Button } from '../Button'
 
 export function ToggleTheme() {
   const { resolvedTheme, setTheme } = useTheme()
+  const isMounted = useIsMounted()
   return (
     <Button variant="icon" onClick={() => setTheme(resolvedTheme === 'light' ? 'dark' : 'light')}>
-      {resolvedTheme === 'light' ? <SunIcon /> : <MoonIcon />}
+      {isMounted && resolvedTheme === 'light' ? <SunIcon /> : <MoonIcon />}
     </Button>
   )
 }
